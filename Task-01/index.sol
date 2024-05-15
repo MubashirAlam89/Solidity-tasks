1492ced352368167b69d30ffa35cad0efd668830
mubashiralam89 <mubashiralam89@gmail.com>
2024-05-15 10:10:30 +0500

    task done

---
+++ Task-01/index.sol
@@ -0,0 +1,17 @@
+// Question: Write a Solidity contract that stores and returns the message "Hello, World!".
+
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract HelloWorld {
+    string public message;
+
+    constructor() {
+        message = "Hello, World!";
+    }
+
+    function getMessage() public view returns (string memory) {
+        return message;
+    }
+}
