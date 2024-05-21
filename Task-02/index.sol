de0f6616a96cec86cc341d329b391da925bff73a
mubashiralam89 <mubashiralam89@gmail.com>
2024-05-21 10:19:38 +0500

    task done

---
+++ Task-02/index.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract SimpleStorage {
+    uint256 private data;
+
+    function store(uint256 _data) public {
+        data = _data;
+    }
+
+    function retrieve() public view returns (uint256) {
+        return data;
+    }
+}
