3ccac5823cb5929ef96078ace906a7f40e5665fa
mubashiralam89 <mubashiralam89@gmail.com>
2024-05-31 11:52:18 +0500

    task done

---
+++ Task-05/index.sol
@@ -0,0 +1,21 @@
+// Question: Create a contract where users can vote for one of two options: "Option A" or "Option B".
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract SimpleVoting {
+    uint256 public votesForA;
+    uint256 public votesForB;
+
+    function voteA() public {
+        votesForA += 1;
+    }
+
+    function voteB() public {
+        votesForB += 1;
+    }
+
+    function getVotes() public view returns (uint256, uint256) {
+        return (votesForA, votesForB);
+    }
+}
