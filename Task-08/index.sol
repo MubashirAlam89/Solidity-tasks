80b9298332b256f2016afdb6ca6c6919d4fbf74f
mubashiralam89 <mubashiralam89@gmail.com>
2024-06-20 12:30:37 +0500

    Task done

---
+++ Task-08/index.sol
@@ -0,0 +1,27 @@
+// Task 8: Simple Token Contract (ERC-20 Basics)
+
+// Question: Create a contract that implements a basic ERC-20-like token with transfer functionality.
+
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract SimpleToken {
+    string public name = "SimpleToken";
+    string public symbol = "STK";
+    uint8 public decimals = 18;
+    uint256 public totalSupply = 1000000 * 10 ** uint256(decimals);
+
+    mapping(address => uint256) public balanceOf;
+
+    constructor() {
+        balanceOf[msg.sender] = totalSupply;
+    }
+
+    function transfer(address to, uint256 value) public returns (bool) {
+        require(balanceOf[msg.sender] >= value, "Insufficient balance");
+        balanceOf[msg.sender] -= value;
+        balanceOf[to] += value;
+        return true;
+    }
+}
