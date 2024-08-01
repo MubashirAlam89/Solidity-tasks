d4d507b41a0e7ed9b38adde11ebb402cbcbee079
mubashiralam89 <mubashiralam89@gmail.com>
2024-08-01 10:39:40 +0500

    task-20

---
+++ Task-20/index.sol
@@ -0,0 +1,4 @@
+// Task 20: Token Transfer with Approval Mechanism
+
+// Question: Implement an ERC-20 token with a transfer approval mechanism that allows a third party to transfer tokens on behalf of the token holder.
+
---
+++ Task-20/readme.md
@@ -0,0 +1,3 @@
+# Task 20: Token Transfer with Approval Mechanism
+
+### Question: Implement an ERC-20 token with a transfer approval mechanism that allows a third party to transfer tokens on behalf of the token holder.
