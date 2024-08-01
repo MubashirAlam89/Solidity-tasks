2ebcbdc959d2042f235694c6dd2bc0451f2c7d8d
mubashiralam89 <mubashiralam89@gmail.com>
2024-08-01 10:40:07 +0500

    task done

--- Task-20/index.sol
+++ Task-20/index.sol
@@ -2,3 +2,40 @@
 
 // Question: Implement an ERC-20 token with a transfer approval mechanism that allows a third party to transfer tokens on behalf of the token holder.
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract ApproveToken {
+    string public name = "ApproveToken";
+    string public symbol = "APT";
+    uint8 public decimals = 18;
+    uint256 public totalSupply = 1000000 * 10 ** uint256(decimals);
+
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
+
+    constructor() {
+        balanceOf[msg.sender] = totalSupply;
+    }
+
+    function transfer(address to, uint256 value) public returns (bool) {
+        require(balanceOf[msg.sender] >= value, "Insufficient balance");
+        balanceOf[msg.sender] -= value;
+        balanceOf[to] += value;
+        return true;
+    }
+
+    function approve(address spender, uint256 value) public returns (bool) {
+        allowance[msg.sender][spender] = value;
+        return true;
+    }
+
+    function transferFrom(address from, address to, uint256 value) public returns (bool) {
+        require(balanceOf[from] >= value, "Insufficient balance");
+        require(allowance[from][msg.sender] >= value, "Allowance exceeded");
+        balanceOf[from] -= value;
+        balanceOf[to] += value;
+        allowance[from][msg.sender] -= value;
+        return true;
+    }
+}
