f70ad964eda5641bc6bbd0f28e46b0db8181b135
mubashiralam89 <mubashiralam89@gmail.com>
2024-08-02 10:43:54 +0500

    task-21

---
+++ Task-21/index.sol
@@ -0,0 +1,5 @@
+// Task 21: Fallback and Receive Functions
+
+// Question: Write a contract that implements both receive and fallback functions. The receive function should be called when ether is sent without data, and the fallback function should log an event if ether is sent with data.
+
+
