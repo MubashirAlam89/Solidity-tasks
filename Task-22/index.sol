08568439d71803eb7d1667cd7a99d8e3256ca844
mubashiralam89 <mubashiralam89@gmail.com>
2024-08-08 10:57:14 +0500

    task-22

---
+++ Task-22/index.sol
@@ -0,0 +1,3 @@
+// Task 22: Reentrancy Attack Prevention
+
+// Question: Write a contract that is vulnerable to a reentrancy attack and then secure it by using ReentrancyGuard. Use OpenZeppelin’s ReentrancyGuard to prevent multiple withdrawals.
