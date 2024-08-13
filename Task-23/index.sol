445883630d0fd1757b1ed68518381a34cfb3a52b
mubashiralam89 <mubashiralam89@gmail.com>
2024-08-13 11:15:56 +0500

    task done

--- Task-23/index.sol
+++ Task-23/index.sol
@@ -2,3 +2,22 @@
 
 // Question: Create an interface ICounter with functions to increment and getCount. Then implement this interface in a contract that allows incrementing a count variable and retrieving its value.
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+interface ICounter {
+    function increment() external;
+    function getCount() external view returns (uint256);
+}
+
+contract Counter is ICounter {
+    uint256 private count;
+
+    function increment() public override {
+        count++;
+    }
+
+    function getCount() public view override returns (uint256) {
+        return count;
+    }
+}
