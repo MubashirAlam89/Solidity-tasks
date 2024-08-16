042c713c0ff1129f9454ee9d14b9fe517f765223
mubashiralam89 <mubashiralam89@gmail.com>
2024-08-16 11:24:55 +0500

    task-24

---
+++ Task-24/index.sol
@@ -0,0 +1,4 @@
+// Task 24: Random Number Generation in Lottery Game
+
+// Question: Write a simple lottery contract where users can buy tickets (by sending ether), and a winner is selected at random. Use block variables as a basic source of randomness.
+
---
+++ Task-24/readme.md
@@ -0,0 +1,3 @@
+# Task 24: Random Number Generation in Lottery Game
+
+### Question: Write a simple lottery contract where users can buy tickets (by sending ether), and a winner is selected at random. Use block variables as a basic source of randomness.
