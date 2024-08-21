dc5e930ee5c0dfa4f1ac5bf6a5cd80e5cbf1385a
mubashiralam89 <mubashiralam89@gmail.com>
2024-08-21 12:31:31 +0500

    task-25

---
+++ Task-25/index.sol
@@ -0,0 +1,4 @@
+// Task 25: Upgradable Contract using Proxy Pattern
+
+// Question: Implement an upgradable contract pattern using a basic proxy contract that delegates calls to a logic contract. Write a simple storage contract and an upgradable proxy to change logic.
+
---
+++ Task-25/readme.md
@@ -0,0 +1,3 @@
+# Task 25: Upgradable Contract using Proxy Pattern
+
+### Question: Implement an upgradable contract pattern using a basic proxy contract that delegates calls to a logic contract. Write a simple storage contract and an upgradable proxy to change logic.
