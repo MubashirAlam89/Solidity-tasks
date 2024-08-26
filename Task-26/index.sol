b0dfaf6315e292e644cc2c1d977121d7eb7d2b96
mubashiralam89 <mubashiralam89@gmail.com>
2024-08-26 12:37:13 +0500

    task done

--- Task-26/index.sol
+++ Task-26/index.sol
@@ -1,3 +1,23 @@
 // Task 26: Gas Optimization Techniques
 
 // Question: Write a contract that demonstrates a few gas optimization techniques. Use uint8 instead of uint256 where possible, and use unchecked for arithmetic operations where overflow is impossible.
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract GasOptimization {
+    uint8 public smallCounter;
+    uint256 public largeCounter;
+
+    // Use unchecked for addition where overflow is not possible
+    function increment(uint8 _value) public {
+        unchecked {
+            smallCounter += _value;
+        }
+    }
+
+    // Use uint8 instead of uint256 if values are small
+    function setLargeCounter(uint8 _value) public {
+        largeCounter = _value;
+    }
+}
