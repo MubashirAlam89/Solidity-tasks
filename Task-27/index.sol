ec230c8e93a23f442cecced52254ee7284522c57
mubashiralam89 <mubashiralam89@gmail.com>
2024-08-27 12:39:50 +0500

    task-27

---
+++ Task-27/index.sol
@@ -0,0 +1,4 @@
+// Task 27: Custom Errors in Solidity
+
+// Question: Write a contract that demonstrates the use of custom errors to save gas. Create a contract with a function that only allows even numbers to be set, and throw a custom error if the condition fails.
+
