695a8c68abe0cc13d1dfb988ae2beca2878668b2
mubashiralam89 <mubashiralam89@gmail.com>
2024-08-29 12:44:10 +0500

    task done

--- Task-28/index.sol
+++ Task-28/index.sol
@@ -1,3 +1,17 @@
 // Task 28: Emitting Events for State Changes
 
 // Question: Write a contract that emits events whenever a state change occurs. Use an event to log the old and new value whenever a variable is updated.
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract EventLogger {
+    uint256 public value;
+
+    event ValueChanged(uint256 oldValue, uint256 newValue);
+
+    function updateValue(uint256 _newValue) public {
+        emit ValueChanged(value, _newValue);
+        value = _newValue;
+    }
+}
