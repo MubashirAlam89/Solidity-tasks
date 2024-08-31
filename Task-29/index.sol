c01ef666ce494ff6e3b88f20170a1e9dc04d28f5
mubashiralam89 <mubashiralam89@gmail.com>
2024-08-31 12:47:17 +0500

    task done

--- Task-29/index.sol
+++ Task-29/index.sol
@@ -1,3 +1,27 @@
 // Task 29: Interacting with an External Contract
 
 // Question: Create a contract that interacts with another contract to retrieve and set data. Assume there is a contract DataStorage with a setData and getData function. Implement a contract that uses DataStorage for storing and retrieving a value.
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+interface IDataStorage {
+    function setData(uint256 _data) external;
+    function getData() external view returns (uint256);
+}
+
+contract ExternalInteractor {
+    address public dataStorageAddress;
+
+    constructor(address _dataStorageAddress) {
+        dataStorageAddress = _dataStorageAddress;
+    }
+
+    function setExternalData(uint256 _data) public {
+        IDataStorage(dataStorageAddress).setData(_data);
+    }
+
+    function getExternalData() public view returns (uint256) {
+        return IDataStorage(dataStorageAddress).getData();
+    }
+}
