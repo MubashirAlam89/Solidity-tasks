ef6f81bb626892e4e09e2fe5882e9611f3a5a792
mubashiralam89 <mubashiralam89@gmail.com>
2024-09-04 12:52:03 +0500

    task done

--- Task-30/index.sol
+++ Task-30/index.sol
@@ -2,3 +2,17 @@
 
 // Question: Write a contract with a modifier that restricts access to a function based on specific conditions. Implement a function that can only be called if the caller’s address is odd.
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract ConditionalAccess {
+    // Modifier to allow only addresses with an odd address value
+    modifier onlyOddAddress() {
+        require(uint160(msg.sender) % 2 != 0, "Caller address is even");
+        _;
+    }
+
+    function restrictedFunction() public onlyOddAddress returns (string memory) {
+        return "Function called by an odd address!";
+    }
+}
