2b119276275f9b7baadad1be588878c26231711c
mubashiralam89 <mubashiralam89@gmail.com>
2024-09-18 11:42:45 +0500

    task done

--- Task-33/index.sol
+++ Task-33/index.sol
@@ -1,3 +1,23 @@
 // Task 33: Creating a Simple Non-Fungible Token (NFT)
 
 // Question: Create a simple NFT contract using the ERC721 standard. Implement minting of a unique token with a unique token ID for each new NFT.
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract SimpleNFT is ERC721 {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor() ERC721("SimpleNFT", "SNFT") {}
+
+    function mintNFT(address recipient) public returns (uint256) {
+        _tokenIds.increment();
+        uint256 newItemId = _tokenIds.current();
+        _mint(recipient, newItemId);
+        return newItemId;
+    }
+}
