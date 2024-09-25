7288cad1d93084d4eb7137c9d9b63f69f33e1eba
mubashiralam89 <mubashiralam89@gmail.com>
2024-09-25 11:56:55 +0500

    task 35

---
+++ Task-35/index.sol
@@ -0,0 +1,6 @@
+// Task 35: Using selfdestruct for Contract Termination
+
+
+// Question: Implement a contract with an owner address that can terminate the contract using selfdestruct. When called, the contract should transfer its balance to the owner and delete itself from the blockchain.
+
+
